fb4b3d92ed6352aaff3650524b6102dd7dd68a81
vasu744896 <vasanthkumar744896@gmail.com>
2025-05-29 21:36:53 +0530

    playwright Automation

--- Playwright/Selenium/install.sh
+++
@@ -1,2 +0,0 @@
- npx @puppeteer/browsers install chromedriver@133.0.6943.98
- # /Users/sivakumar/Downloads/chromedriver/mac_arm-133.0.6943.98/chromedriver-mac-arm64/chromedriver
--- Playwright/Selenium/run.sh
+++
@@ -1,5 +0,0 @@
-rm -r src/temp
-export JAVA_HOME=$(/usr/libexec/java_home -v 17)
-mvn compile exec:java -Dexec.mainClass="ai.TestCaseGenerator"
-# sleep(10)
-# mvn clean install test
